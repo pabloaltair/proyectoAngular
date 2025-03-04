3bfd76ee1f39bfce075a78fcb04aa9bc0fff8456
pabloaltair <csi24-prosdez@colegioaltair.net>
2025-03-04 10:02:13 +0100

    Correcion, ultima version antes recreo

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,23 +1,12 @@
-import { Component, OnInit, OnDestroy, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   imports: [RouterLink],
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrl: './menu.component.css'
 })
-export class MenuComponent implements OnInit, OnDestroy {
+export class MenuComponent {
 
-  constructor(private renderer: Renderer2) { }
-
-  ngOnInit(): void {
-    // Agrega la clase que define el fondo deseado al body
-    this.renderer.addClass(document.body, 'login-background');
-  }
-
-  ngOnDestroy(): void {
-    // Remueve la clase cuando el componente se destruye
-    this.renderer.removeClass(document.body, 'login-background');
-  }
 }
