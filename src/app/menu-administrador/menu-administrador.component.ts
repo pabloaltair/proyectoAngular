94470293e8f0d744c5008ffbd674183d7c31bb3a
pabloaltair <csi24-prosdez@colegioaltair.net>
2025-03-06 12:11:09 +0100

    Crud completo Funcional

--- src/app/menu-administrador/menu-administrador.component.ts
+++ src/app/menu-administrador/menu-administrador.component.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
-import { ModificarUsuarioComponent } from "../modificar-usuario/modificar-usuario.component";
-import { EliminarUsuarioComponent } from "../eliminar-usuario/eliminar-usuario.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu-administrador',
+  imports: [RouterLink],
   templateUrl: './menu-administrador.component.html',
-  styleUrls: ['./menu-administrador.component.css'],
-  imports: [ModificarUsuarioComponent, EliminarUsuarioComponent]
+  styleUrl: './menu-administrador.component.css'
 })
 export class MenuAdministradorComponent {
-  activeSection: string = 'modificar'; // Sección activa por defecto
 
-  constructor() {}
-
-  // Método para actualizar la sección activa
-  setActiveSection(section: string) {
-    this.activeSection = section;
-  }
 }
